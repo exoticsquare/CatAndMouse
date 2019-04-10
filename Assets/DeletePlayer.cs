8b74fa1510690942263df75f2398f5333da15a02
DanteTester <Dantezworld@gmail.com>
2019-04-10 17:39:39 -0400

    I did a thing

---
+++ Assets/DeletePlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeletePlayer : MonoBehaviour
+{
+    public GameObject Player;
+
+    // Update is called once per frame
+    void DestroyPlayer()
+    {
+        Player.SetActive(false);   
+    }
+}
---
+++ Assets/MouseClicker.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MouseClicker : MonoBehaviour
+{
+    public int numberOfHearts;
+    public int health;
+    public Image[] hearts;
+    public Sprite FullHearts;
+    public Sprite EmptyHearts;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < health)
+            {
+                hearts[i].sprite = FullHearts;
+            }
+            else
+            {
+                hearts[i].sprite = EmptyHearts;
+            }
+
+            if (i < numberOfHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Get the mouse position on the screen and send a raycast into the game world from that position.
+            Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
+
+             //If something was hit, the RaycastHit2D.collider will not be null.
+             if (hit.collider != null)
+             {
+                Destroy(GameObject.Find("Player"));
+             }
+             else
+             {
+                 health = health-1;
+                 Debug.Log(health);
+             }
+        }
+        
+    }
+}
